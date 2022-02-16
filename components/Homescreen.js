91186a3faf696f958dfdb10055d29c60d995546c
dakotawlacy <dwl0010@mix.wvu.edu>
2022-02-16 15:28:48 -0500

    Changed to fit ipad

--- components/Homescreen.js
+++ components/Homescreen.js
@@ -1,6 +1,6 @@
 // Homescreen.js
 import React, { Component } from 'react';
-import {StyleSheet, Button, View, Text, Image } from 'react-native';
+import {StyleSheet, Button, View, Text, ImageBackground, Image } from 'react-native';
 import { textDecorationColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 import { isRequired } from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedColorPropType';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
@@ -10,46 +10,50 @@ export default class Homescreen extends Component {
   render() {
     return (
       <View >
-        <Image style= {{width: '100%', height: '110%'}} source ={require("../assets/blankBackground.png")}  />
+        <ImageBackground source={require('../assets/blankBackground.png')} style ={{width: "100%", height: "120%"}}>
         
-        <View View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: "70%", justifyContent: 'center', alignItems: 'center'}}>
-          <Text style = {{color: 'white',fontSize: 40}}>Welcome!</Text>
+        
+
+        <View View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: "65%", justifyContent: 'center', alignItems: 'center'}}>
+        <Image style={{width: 650, height: 200, }} source={require('../assets/ecocarlogo.png')}/>
         </View>
-        <View View style={{position: 'absolute', top: 0, left: '15%', right: '15%', bottom: '35%', justifyContent: 'center', alignItems: 'center', marginTop: '30%'}}>
-          <Text style = {{color: 'white',fontSize: 18, textAlign:'center',padding:5}}>Welcome to the West Virginia University Ecocar’s Vehicle and Adaptive Cruise Control
+        <View View style={{position: 'absolute', top: 0, left: '15%', right: '15%', bottom: '45%', justifyContent: 'center', alignItems: 'center', marginTop: '30%'}}>
+          <Text style = {{color: 'white',fontSize: 25, textAlign:'center',padding:5}}>Welcome to the West Virginia University Ecocar’s Vehicle and Adaptive Cruise Control
 
 educational app!
 </Text>
-<Text style = {{color: 'white',fontSize: 18, textAlign:'center',padding:5}}>
+<Text style = {{color: 'white',fontSize: 25, textAlign:'center',padding:5}}>
 
 The Ecocar Mobility Challenge is a multi-year, multi-university competition focusing on
 innovation in the automotive industry, currently focusing transforming a 2019 Chevrolet Blazer
 
 into a hybrid vehicle that utilizes Adaptive Cruise Control (ACC).
-
-To learn more about our EcoCar, use the buttons below to check out our Symbol Dictionary,
+{"\n"}
+{"\n"}
+To learn more about the WVU EcoCar, use the buttons below to check out our Symbol Dictionary,
 ACC Educational Video, or an interactive wheel to learn.
 </Text>
         </View>
-        <View style={{position:'absolute', bottom:'20%',left:'4%',width: '45%'}}>
-          <Button color= "#EAAA00"
+        <View style={{position:'absolute', bottom:'40%',left:'4%',width: '45%',backgroundColor:'#EAAA00',borderRadius: 20}}>
+          <Button color= "#black"
             title="Symbol Dictionary"
             onPress={() => this.props.navigation.navigate('Symbol Dictionary')}
           />
           </View>
-          <View style={{position:'absolute',bottom:'20%',right:'5%',width: '45%'}}>
-          <Button color= "#EAAA00"
+          <View style={{position:'absolute',bottom:'40%',right:'5%',width: '45%', backgroundColor: '#EAAA00', borderRadius: 20}}>
+          <Button color= "black"
             title="EcoCar Video"
             onPress={() => this.props.navigation.navigate('EcoCar Video')}
           />
           </View>
-          <View style={{position:'absolute',bottom:'12%',right:'30%',width: '40%'}}>
-          <Button color= "#EAAA00"
+          <View style={{position:'absolute',bottom:'35%',right:'30%',width: '40%', backgroundColor: '#EAAA00', borderRadius: 20}}>
+          <Button 
+            color= "#black"
             title="Interactive Wheel"
             onPress={() => this.props.navigation.navigate('Interactive Wheel')}
           />
           </View>
-          
+          </ImageBackground>
        </View>
 
     );
--- components/VideoScreen.js
+++ components/VideoScreen.js
@@ -15,15 +15,15 @@ export default class VideoScreen extends Component {
        <Image style= {{width: '100%', height: '100%'}} source ={require("../assets/blankBackground.png")}/>
         <View View style = {{position: 'absolute', top: '-50%', left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center',}}>
 
-        <Text style={{ fontWeight: 'bold', color: '#EAAA00', fontSize: 30, paddingBottom: 40 }}>
+        <Text style={{ fontWeight: 'bold', color: '#EAAA00', fontSize: 45, paddingBottom: 40 }}>
          Safety Module Video
         </Text>
         
         </View>
-        <View style ={{position: 'absolute', top: '30%', left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center', backgroundColor: '#EAAA00', height: '30%', width: "100%",}}>
+        <View style ={{position: 'absolute', top: '30%', left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center', backgroundColor: '#EAAA00', height: '40%', width: "100%",}}>
 
         <YoutubePlayer
-        height = '86%'
+        height = '93%'
         width = '97%'
         videoId = {'g5d_xUb0US8'}
         />
@@ -32,7 +32,7 @@ export default class VideoScreen extends Component {
         
 
         
-        <View style ={{position: 'absolute', top: '65%', left: '37.5%', right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center', backgroundColor: '#EAAA00', height: '5%', width: "25%", borderRadius: '25%'}}>
+        <View style ={{position: 'absolute', top: '75%', left: '37.5%', right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center', backgroundColor: '#EAAA00', height: '5%', width: "25%", borderRadius: "40"}}>
         
         <Button 
         title="Take Quiz"
