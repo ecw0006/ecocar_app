2373df5317b6fa9a71af7b2ca2949bf89b34a883
dakotawlacy <dwl0010@mix.wvu.edu>
2022-03-23 15:45:25 -0400

    quiz page

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, View, Text,StyleSheet} from 'react-native';
+import { Button, View, Text,StyleSheet, ScrollView, ImageBackground} from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 
@@ -7,135 +7,338 @@ import BouncyCheckbox from "react-native-bouncy-checkbox";
 export default class Quiz extends Component {
   render() {
     return (
+      <ScrollView>
+        
+        <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: "#002855"}}>
+        
+
+          <View style={{width: '95%', paddingTop:50, paddingBottom:50,}}>
+            <Text style = {{color: '#FFFFFF',fontSize: 25, textAlign:'center',paddingBottom: 10}}>What is ACC?</Text>
+            <View style= {{marginLeft: '35%'}}>
+              <View style= {{paddingBottom: 5}}>
+              <BouncyCheckbox
+                size={25}
+                fillColor="#FFFFFF"
+                unfillColor="#FFFFFF"
+                text="Adaptive Cruise Control is a system that accelerates only."
+                textStyle={{textDecorationLine: "none"}}
+                iconStyle={{ borderColor: "#FFFFFF" }}
+                onPress={(isChecked) => {
+                  alert("This is partially correct, but ACC has more than just one function.");
+              }}
+              />
+              </View>
+
+              <View style= {{paddingBottom: 5}}>
+              <BouncyCheckbox
+                size={25}
+                fillColor="FFFFFF"
+                unfillColor="#FFFFFF"
+                text="Adaptive Cruise Control is a system that breaks suddenly."
+                textStyle={{textDecorationLine: "none"}}
+                iconStyle={{ borderColor: "#FFFFFF" }}
+                onPress={(isChecked) => {
+                  alert("This is partially correct, but ACC has more than just one function.");
+                }}
+              />
+              </View>
+
+              <View style= {{paddingBottom: 5}}>
+              <BouncyCheckbox
+                size={25}
+                fillColor="green"
+                unfillColor="#FFFFFF"
+                text="Adaptive Cruise Control is a system that automatically accelerates or breaks."
+                textStyle={{textDecorationLine: "none"}}
+                iconStyle={{ borderColor: "#FFFFFF" }}
+                onPress={(isChecked) => {
+                  alert("Correct! Good Job");
+                }}
+              />
+              </View>
+              
+              <View>
+              <BouncyCheckbox
+                size={25}
+                fillColor="#FFFFFF"
+                unfillColor="#FFFFFF"
+                text="None of the Above."
+                textStyle={{textDecorationLine: "none"}}
+                iconStyle={{ borderColor: "#FFFFFF" }}
+                onPress={(isChecked) => {
+                  alert("One of the answers above is correct.");
+                }}
+              />
+              </View>
+            </View>
+          </View>
+
+          <View style={{width: '95%', paddingTop:50, paddingBottom:50,}}>
+          <Text style = {{color: '#FFFFFF',fontSize: 25, textAlign:'center',paddingBottom: 10}}>How do you turn on Cruise Control?</Text>
+    
+            <View style= {{marginLeft: '35%',}}> 
+          <View style= {{paddingBottom: 5}}>
+          <BouncyCheckbox
+    size={25}
+    fillColor="green"
+    unfillColor="#FFFFFF"
+    text="Press the button on the steering wheel labeled Cruise Control."
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("Correct! Great Job");
       
-      <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-        <View style={{width: '95%', paddingTop:50, paddingBottom:50}}>
-        <Text style = {{color: 'black',fontSize: 25, textAlign:'center',paddingBottom: 10}}>What does ACC stand for?</Text>
-        <View style= {{marginLeft: '35%'}}>
-        <View style= {{paddingBottom: 5}}>
-        <BouncyCheckbox
-        
-  size={25}
-  fillColor="#FFFFFF"
-  unfillColor="#FFFFFF"
-  text="Automatic Cruise Control"
-  textStyle={{textDecorationLine: "none"}}
-  iconStyle={{ borderColor: "black" }}
-  onPress={(isChecked) => {
-    alert("Incorrect");
-    
-  }}
-/>
-</View>
-<View style= {{paddingBottom: 5}}>
-<BouncyCheckbox
-  size={25}
-  fillColor="FFFFFF"
-  unfillColor="#FFFFFF"
-  text="Automotive Crash Control"
-  textStyle={{textDecorationLine: "none"}}
-  iconStyle={{ borderColor: "black" }}
-  onPress={(isChecked) => {
-    alert("Incorrect");
-  }}
-/>
-</View>
-<View style= {{paddingBottom: 5}}>
-<BouncyCheckbox
-  size={25}
-  fillColor="green"
-  unfillColor="#FFFFFF"
-  text="Adaptive Cruise Control"
-  textStyle={{textDecorationLine: "none"}}
-  iconStyle={{ borderColor: "black" }}
-  onPress={(isChecked) => {
-    alert("Correct");
-  }}
-/>
-</View>
+    }}
+  />
+  </View>
+  <View style= {{paddingBottom: 5}}>
+  <BouncyCheckbox
+    size={25}
+    fillColor="FFFFFF"
+    unfillColor="#FFFFFF"
+    text="Insert the Cruise Control disk into the CD player."
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("Not quite, there is a button you press to turn on ACC.");
+    }}
+  />
+  </View>
+  <View style= {{paddingBottom: 5}}>
+  <BouncyCheckbox
+    size={25}
+    fillColor="#FFFFFF"
+    unfillColor="#FFFFFF"
+    text="Tap the gas petal 3 times."
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("Not quite, there is a button you press to turn on ACC.");
+    }}
+  />
+  </View>
 
-<BouncyCheckbox
-  size={25}
-  fillColor="#FFFFFF"
-  unfillColor="#FFFFFF"
-  text="A Cool Car"
-  textStyle={{textDecorationLine: "none"}}
-  iconStyle={{ borderColor: "black" }}
-  onPress={(isChecked) => {
-    alert("Incorrect");
-  }}
-/>
-</View>
+  <BouncyCheckbox
+    size={25}
+    fillColor="#FFFFFF"
+    unfillColor="#FFFFFF"
+    text="None of the above."
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("One of the above questions is correct.");
+    }}
+  />
+  </View>
+  
 
 
-    
+      
+          </View>
+
+          <View style={{width: '95%', paddingTop:50, paddingBottom:50,}}>
+          <Text style = {{color: '#FFFFFF',fontSize: 25, textAlign:'center',paddingBottom: 10}}>In what situations should ACC be used?</Text>
+          
+          <View style= {{marginLeft: '35%',}}> 
+          
+  <View style= {{paddingBottom: 5}}>
+  <BouncyCheckbox
+    size={25}
+    fillColor="FFFFFF"
+    unfillColor="#FFFFFF"
+    text="In heavy traffic."
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("Not Quite, The ACC should only be used in safe situations.");
+    }}
+  />
+  </View>
+  <View style= {{paddingBottom: 5}}>
+  <BouncyCheckbox
+    size={25}
+    fillColor="#FFFFFF"
+    unfillColor="#FFFFFF"
+    text="In the rain and snow."
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("Not Quite, The ACC should only be used in safe situations.");
+    }}
+  />
+  </View>
+
+  <View style= {{paddingBottom: 5}}>
+          <BouncyCheckbox
+    size={25}
+    fillColor="green"
+    unfillColor="#FFFFFF"
+    text="Only when roads are clear and in good condition."
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("Correct! Good Job");
+      
+    }}
+  />
+  </View>
+
+  <BouncyCheckbox
+    size={25}
+    fillColor="#FFFFFF"
+    unfillColor="#FFFFFF"
+    text="You can use it any time."
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("Not Quite, There are certain situations that are unfavorable for ACC.");
+    }}
+  />
+  </View>
+  
+
+
+      
+          </View>
+
+          <View style={{width: '95%', paddingTop:50, paddingBottom:50,}}>
+          <Text style = {{color: '#FFFFFF',fontSize: 25, textAlign:'center',paddingBottom: 10}}>Why is ACC useful?</Text>
+          
+          <View style= {{marginLeft: '35%',}}> 
+         
+  <View style= {{paddingBottom: 5}}>
+  <BouncyCheckbox
+    size={25}
+    fillColor="FFFFFF"
+    unfillColor="#FFFFFF"
+    text="It does not require a driver in the car to operate."
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("Not quite, the driver should always be present in the driver seat while the vehicle is operating.");
+    }}
+  />
+  </View>
+
+  <View style= {{paddingBottom: 5}}>
+          <BouncyCheckbox
+    size={25}
+    fillColor="green"
+    unfillColor="#FFFFFF"
+    text="Its a safe system that maintains distance from the vehicle in front of you."
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("Correct! Good Job");
+      
+    }}
+  />
+  </View>
+
+  <View style= {{paddingBottom: 5}}>
+  <BouncyCheckbox
+    size={25}
+    fillColor="#FFFFFF"
+    unfillColor="#FFFFFF"
+    text="It works without fuel in the car."
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("Not Quite, ACC is more focused on safety.");
+    }}
+  />
+  </View>
+
+  <BouncyCheckbox
+    size={25}
+    fillColor="#FFFFFF"
+    unfillColor="#FFFFFF"
+    text="It will teach people how to drive."
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("Not Quite, ACC is more focused on safety rather than teaching.");
+    }}
+  />
+  </View>
+  
+
+
+      
+          </View>
+
+          <View style={{width: '95%', paddingTop:50, paddingBottom:50,}}>
+          <Text style = {{color: '#FFFFFF',fontSize: 25, textAlign:'center',paddingBottom: 10}}>Pressing down on the break will deactivate ACC.</Text>
+          
+          <View style= {{marginLeft: '35%',}}> 
+          <View style= {{paddingBottom: 5}}>
+          <BouncyCheckbox
+    size={25}
+    fillColor="green"
+    unfillColor="#FFFFFF"
+    text="True"
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("Correct");
+      
+    }}
+  />
+  </View>
+  <View style= {{paddingBottom: 5}}>
+  <BouncyCheckbox
+    size={25}
+    fillColor="FFFFFF"
+    unfillColor="#FFFFFF"
+    text="False"
+    textStyle={{textDecorationLine: "none"}}
+    iconStyle={{ borderColor: "#FFFFFF" }}
+    onPress={(isChecked) => {
+      alert("Incorrect");
+    }}
+  />
+  </View>
+  
+
+  
+  </View>
+  
+
+
+      
+          </View>
+
+          
         </View>
 
-        <View style={{width: '95%'}}>
-        <Text style = {{color: 'black',fontSize: 25, textAlign:'center',paddingBottom: 10}}>What is the model of this years EcoCar?</Text>
-        
-        <View style= {{marginLeft: '35%'}}> 
-        <View style= {{paddingBottom: 5}}>
-        <BouncyCheckbox
-  size={25}
-  fillColor="green"
-  unfillColor="#FFFFFF"
-  text="Blazer"
-  textStyle={{textDecorationLine: "none"}}
-  iconStyle={{ borderColor: "black" }}
-  onPress={(isChecked) => {
-    alert("Correct");
-    
-  }}
-/>
-</View>
-<View style= {{paddingBottom: 5}}>
-<BouncyCheckbox
-  size={25}
-  fillColor="FFFFFF"
-  unfillColor="#FFFFFF"
-  text="Equinox"
-  textStyle={{textDecorationLine: "none"}}
-  iconStyle={{ borderColor: "black" }}
-  onPress={(isChecked) => {
-    alert("Incorrect");
-  }}
-/>
-</View>
-<View style= {{paddingBottom: 5}}>
-<BouncyCheckbox
-  size={25}
-  fillColor="#FFFFFF"
-  unfillColor="#FFFFFF"
-  text="Trailblazer"
-  textStyle={{textDecorationLine: "none"}}
-  iconStyle={{ borderColor: "black" }}
-  onPress={(isChecked) => {
-    alert("Incorrect");
-  }}
-/>
-</View>
-
-<BouncyCheckbox
-  size={25}
-  fillColor="#FFFFFF"
-  unfillColor="#FFFFFF"
-  text="Tahoe"
-  textStyle={{textDecorationLine: "none"}}
-  iconStyle={{ borderColor: "black" }}
-  onPress={(isChecked) => {
-    alert("Incorrect");
-  }}
-/>
-</View>
- 
-
-    
+        {/* <View style={{flexDirection:'row',backgroundColor:'red', height:250,padding:5}}>
+          <View style={{backgroundColor:'blue', flex:1,alignItems:'center',justifyContent:'center'}}>
+            <Text>Symbol</Text>
+          </View>
+          <View style={{backgroundColor:'green',flex:2,alignItems:'center',justifyContent:'center'}}>
+            <Text>this is about the acc</Text>
+          </View>
         </View>
 
-      </View>
+        <View style={{flexDirection:'row',backgroundColor:'red', height:250,padding:5}}>
+          <View style={{backgroundColor:'blue', flex:1}}>
+            <Text>asdfs</Text>
+          </View>
+          <View style={{backgroundColor:'green',flex:2}}>
 
+          </View>
+        </View>
 
+        <View style={{flexDirection:'row',backgroundColor:'red', height:250,padding:5}}>
+          <View style={{backgroundColor:'blue', flex:1}}>
+            <Text>asdfs</Text>
+          </View>
+          <View style={{backgroundColor:'green',flex:2}}>
+
+          </View>
+        </View> */}
+
+      </ScrollView>
 
 
     )
