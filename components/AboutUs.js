bb20b9d9e4762b6af2ca1780683bdfa34c2d7c1f
Emily Waechter <ecw0006@mix.wvu.edu>
2022-05-02 23:58:42 -0400

    fix background

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -10,7 +10,7 @@ export default class AboutUs extends Component {
   render() {
     return (
       <View >
-        <Image style= {{width: '100%', height: '110%'}} source ={require("../assets/blankhomeScreenv2.png")}  />
+        <Image style= {{width: '100%', height: '100%'}} source ={require("../assets/blankhomeScreenv2.png")}  />
         
         <View View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: "60%", justifyContent: 'center', alignItems: 'center'}}>
           <Text style = {{color: 'white',fontSize: 40}}>About Us!</Text>
